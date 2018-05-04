6461c2253c750d6116daf6a0505d2fe4036f7418
jean_philippe_59184 <jpcatel1979@gmail.com>
2018-05-04 15:25:46 +0200

    2018-05-04

---
+++ src/main/java/com/formation/boutique/rest/BoutiqueRestApplication.java
@@ -0,0 +1,12 @@
+package com.formation.boutique.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiqueRestApplication.class, args);
+	}
+}
